a54c33edd99ae1a9c73d166487f7c51a386b9872
Katherina <fedoseeva2292@gmail.com>
2022-01-14 16:01:24 +0100

    Rightbar UI

--- src/components/Rightbar/Rightbar.jsx
+++ src/components/Rightbar/Rightbar.jsx
@@ -3,7 +3,88 @@ import './rightbar.css';
 export const Rightbar = () => {
   return (
     <div className="rightbar">
-      RIGHTBAR
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img
+            src="/assets/icons/gift.png"
+            alt="birthdayImg"
+            className="birthdayImg"
+          />
+          <span className="birthdayText">
+            <b>Angelika Smith</b> and <b>other 2 friends</b> have a birthday!
+          </span>
+        </div>
+        <img src="/assets/ad.jpeg" alt="ad" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/user4.jpeg"
+                alt="friend-profile-img"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Kessady</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/user4.jpeg"
+                alt="friend-profile-img"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Kessady</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/user4.jpeg"
+                alt="friend-profile-img"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Kessady</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/user4.jpeg"
+                alt="friend-profile-img"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Kessady</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/user4.jpeg"
+                alt="friend-profile-img"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Kessady</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="/assets/person/user4.jpeg"
+                alt="friend-profile-img"
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Kessady</span>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 };
