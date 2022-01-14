1ab010c1bb81836efdb82a19e6076f7537704475
Katherina <fedoseeva2292@gmail.com>
2022-01-14 11:02:56 +0100

    Share UI

---
+++ src/components/Share/Share.jsx
@@ -0,0 +1,44 @@
+import './share.css';
+import { PermMedia, Tag, LocationOn, Mood } from '@mui/icons-material';
+
+export const Share = () => {
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <img
+            src="/assets/person/profile.jpeg"
+            alt="profileImg"
+            className="shareProfileImg"
+          />
+          <input
+            placeholder="What's in your mind Katharina?"
+            className="shareInput"
+          />
+        </div>
+        <hr className="shareHr" />
+        <div className="shareBottom">
+          <div className="shareOptions">
+            <div className="shareOption">
+              <PermMedia htmlColor="tomato" className="shareIcon" />
+              <span className="shareOptionText">Photo or Video</span>
+            </div>
+            <div className="shareOption">
+              <Tag htmlColor="green" className="shareIcon" />
+              <span className="shareOptionText">Tag</span>
+            </div>
+            <div className="shareOption">
+              <LocationOn className="shareIcon" />
+              <span className="shareOptionText">Location</span>
+            </div>
+            <div className="shareOption">
+              <Mood htmlColor="orange" className="shareIcon" />
+              <span className="shareOptionText">Feelings</span>
+            </div>
+          </div>
+          <button className="shareButton">Share</button>
+        </div>
+      </div>
+    </div>
+  );
+};
