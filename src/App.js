426ec2a206e6ea54af996c885cfe9c15cc0fe153
Katherina <fedoseeva2292@gmail.com>
2022-01-26 10:53:20 +0100

    header 2.0

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import { Box, Grid } from '@mui/material';
+import './app.css';
+import { Header } from './components/Header/Header';
+import { Navbar } from './components/Navbar/Navbar';
 import { Home } from './pages/home/Home';
 import { Login } from './pages/login/Login';
 import { Profile } from './pages/profile/Profile';
@@ -6,10 +10,10 @@ import { Signup } from './pages/signup/Signup';
 export const App = () => {
   return (
     <div>
-      <Home />
-      {/* <Profile /> */}
-      {/* <Login /> */}
-      {/* <Signup /> */}
+      <Header />
+      <Grid container>
+        
+      </Grid>
     </div>
   );
 };
