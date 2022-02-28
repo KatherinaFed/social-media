b69f32864b6669da5de74646e9854a143b902aeb
Katherina <fedoseeva2292@gmail.com>
2022-02-28 19:01:09 +0100

    README, preloader - some fixes

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,7 +1,9 @@
+import preloaderGif from '../../assets/preloader.gif';
+
 export const Preloader = () => {
   return (
     <div style={{ position: 'absolute', margin: '20% 30% 30% 30%' }}>
-      <img src="/assets/preloader.gif" alt="preloader" />
+      <img src={preloaderGif} alt="preloader" />
     </div>
   );
 };
